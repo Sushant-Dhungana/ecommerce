2ee655bdd14dd1adbbacb62945c4449646f580d5
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-08-27 16:43:20 +0545

    layout for metatag and footer and header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Home",
+  description: "Modern E-commerce Platform For Your Kinda Business",
+};
+export default function Home() {
+  return (
+    <div>
+      Ecommerce
+      <Button variant="default">Button</Button>
+    </div>
+  );
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "E-Commerce";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Modern E-commerce Platform For Your Kinda Business";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/";
