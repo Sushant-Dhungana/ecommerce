fa1519a5c05f70f96416b15c3adc2593f47cca00
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-08-29 14:26:49 +0545

    product types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,18 +3,9 @@ import { getLatestProducts } from "@/lib/actions/product.actions";
 export default async function Home() {
   const latestProducts = await getLatestProducts();
 
-  // Transform rating from string to number
-  const transformedProducts = latestProducts.map((product) => ({
-    ...product,
-    rating:
-      product.rating && !isNaN(parseFloat(product.rating))
-        ? parseFloat(product.rating)
-        : 0,
-  }));
-
   return (
     <div>
-      <ProductList data={transformedProducts} title="Newest Arrivals" />
+      <ProductList data={latestProducts} title="Newest Arrivals" />
     </div>
   );
 }
