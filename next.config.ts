d89c63bc2750545928690fd9e709886106b31567
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-08-30 12:44:55 +0545

    eslint error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
   eslint: {
     // Ignore ESLint errors during builds
     ignoreDuringBuilds: true,
@@ -11,12 +8,6 @@ const nextConfig: NextConfig = {
   typescript: {
     // Keep type checking for your actual code
     ignoreBuildErrors: false,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./scripts/generate-prisma");
-    }
-    return config;
   },
 };
 
