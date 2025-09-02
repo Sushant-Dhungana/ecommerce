22dd52dd1e415443f60cae947d3398ab31d55961
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-09-02 18:11:35 +0545

    transition state and spinner in cart buttons

--- components/product/add-to-cart.tsx
+++ components/product/add-to-cart.tsx
@@ -3,33 +3,40 @@
 import { Cart, CartItem } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Minus, Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { toast } from "sonner";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isPendingRemove, startTransitionRemove] = useTransition();
   const handleAddToCart = async () => {
-    const res = await addItemToCart({ data: item });
-    if (res.success) {
-      toast.success("Item added to cart");
-      //   router.push("/cart");
-    } else {
-      toast.error(res.message);
-    }
-    //handle success cart add item
-    toast(res.message);
+    startTransition(async () => {
+      const res = await addItemToCart({ data: item });
+      if (res.success) {
+        toast.success("Item added to cart");
+        //   router.push("/cart");
+      } else {
+        toast.error(res.message);
+      }
+      //handle success cart add item
+      toast(res.message);
+    });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransitionRemove(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (res) {
-      toast.success(`${item.name} removed from cart`);
-    } else {
-      toast.error(`Cannot remove${item.name}  from cart`);
-    }
-    return;
+      if (res) {
+        toast.success(`${item.name} removed from cart`);
+      } else {
+        toast.error(`Cannot remove${item.name}  from cart`);
+      }
+      return;
+    });
   };
 
   //check if items is in cart
@@ -38,16 +45,30 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div className="flex gap-2 items-center">
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPendingRemove ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
+
       <span>{existItem?.qty}</span>
+
       <Button type="button" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      Add to cart
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <h1>Add to cart</h1>
+      )}
     </Button>
   );
 };
